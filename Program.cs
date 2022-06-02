ffe3200fc3b4ea81f61a554470f8ba305c612ec1
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-06-02 21:41:33 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(3 + 3);
+
+int x = 5;
+Console.WriteLine("I got " + x + " on it");
+
+string f = "Panos";
+string l = "Sneako";
+
+
+int myint = 11;
+double myfloat = myint;
+
+Console.WriteLine(f + l);
+Console.WriteLine(myfloat.GetType());
+
+// int myNum = 5;
+// double myDoubleNum = 5.99D;
+// char myLetter = 'D';
+// bool myBool = true;
+// string myText = "Hello";
